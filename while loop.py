ffe8ef62ea7a0c8893907889276e591be88e7d2e
KIRANMAYI <gkiranmayi19092007@gmail.com>
2025-09-01 10:22:01 +0530

    Add files via upload
    while loop

---
+++ while loop.py
@@ -0,0 +1,24 @@
+c=0
+while True:
+    c=c+1
+    print(c)
+    if c==20:
+        break
+
+while True:
+    print("hi")
+    print("hello")
+    break
+
+i=0
+while True:
+    i=i+1
+    print("hi")
+    if i==10:
+        break
+
+name=input("enter name")
+while name=="":
+    print("please enter the name")
+    name=input("enter name:")
+ 
