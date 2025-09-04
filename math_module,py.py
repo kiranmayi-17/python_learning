a53d7a7b79e9337eb6a831b3dbd759526fc68f21
KIRANMAYI <gkiranmayi19092007@gmail.com>
2025-09-04 09:36:02 +0530

    Add files via upload

---
+++ math_module,py.py
@@ -0,0 +1,30 @@
+#math_module.py
+import math
+n=int(input("enter a number:"))
+print("sqaureroot of",n,"is:",math.sqrt(n))
+
+#factorial
+n=int(input("enter a number:"))
+print("factorial of",n,"is:",math.factorial(n))
+
+#exponentiation
+n=int(input("enter a number:"))
+print("exponentiation of",n,"is:",math.exp(n))
+
+#powers
+x=int(input("enter the 1st number:"))
+y=int(input("enter the 2nd number:"))
+print("return x raised to the power y",n,"is:",math.pow(x,y))
+
+#fabs
+x=int(input("enter a number:"))
+print("returns absolute of float x",n,"is:",math.fabs(x))
+
+
+#gcd
+x=int(input("enter the 1st number:"))
+y=int(input("enter the 2nd number:"))
+print("return gcd of x,y",n,"is:",math.gcd(x,y))
+
+
+
