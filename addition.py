86b68b84de94b8cfb5795e21311caeb860ebe7e4
KIRANMAYI <gkiranmayi19092007@gmail.com>
2025-09-08 14:15:01 +0530

    Add files via upload
    arithmetic operators

---
+++ addition.py
@@ -0,0 +1,10 @@
+#addition.py
+#Approach-1 example
+def Add_Two_Values(a,b):
+    c=a*b
+    print("Sum of two values is=",c)
+#Main program to call function
+#reading INPUT
+    x=int(input("Enter 1st value:"))
+    y=int(input("Enter 2nd value:"))
+    Add_Two_Values(a,b)
