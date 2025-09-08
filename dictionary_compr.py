8cbdc7519209e3357668c4e9350415852ae10495
KIRANMAYI <gkiranmayi19092007@gmail.com>
2025-09-08 14:17:15 +0530

    Add files via upload
    dictionary comprehensions

---
+++ dictionary_compr.py
@@ -0,0 +1,10 @@
+students=["Alice","Bob","Charlie"]
+marks=[85,92,78]
+marks_dict={student:mark for student,mark in zip(students,marks)}
+print(marks_dict) #output:{'Alice':85.'Bob':92,'Charlie':78}
+
+students=["madhu","rama","kiran"]
+marks=[80,95,60]
+marks_dict={student:mark for student,mark in zip(students,marks)}
+print(marks_dict)
+  
